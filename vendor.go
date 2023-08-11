506ae94905d2aed0ab1e7093108ecd9b8636c27c
tobyzxj <toby.zxj@gmail.com>
2023-08-11 20:12:31 +0800

    添加 mqtt broker vendor

---
+++ vendor.go
@@ -0,0 +1,14 @@
+package mqttx
+
+// MQTT服务软件厂商 mosquitto/emqx/mosca/vernemq/rabbitmq/aliyun/qcloud/baidu/jd/...
+const (
+	MQTT_BROKER_MOSQUITTO = "mosquitto"
+	MQTT_BROKER_EMQX      = "emqx"
+	MQTT_BROKER_MOSCA     = "mosca"
+	MQTT_BROKER_VERNEMQ   = "vernemq"
+	MQTT_BROKER_RABBITMQ  = "rabbitmq"
+	MQTT_BROKER_ALIYUN    = "aliyun"
+	MQTT_BROKER_QCLOUD    = "qcloud"
+	MQTT_BROKER_BAIDU     = "baidu"
+	MQTT_BROKER_JD        = "jd"
+)
